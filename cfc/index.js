0331ee73132b469d4b62681f780b2dd59f581291
ilBarbogio <barbogioster.com>
2025-06-18 20:52:47 +0200

    added label to see the palette

--- cfc/index.js
+++ cfc/index.js
@@ -1,21 +1,27 @@
 function changeColor(ev){
   switch(ev.target.getAttribute("kind")){
     case "design":
+      document.querySelector("label.design").innerHTML=ev.target.value
       document.body.style.setProperty("--design-color",ev.target.value)
       break
     case "designLight":
+      document.querySelector("label.designLight").innerHTML=ev.target.value
       document.body.style.setProperty("--design-color-light",ev.target.value)
       break
     case "fold":
+      document.querySelector("label.fold").innerHTML=ev.target.value
       document.body.style.setProperty("--fold-color",ev.target.value)
       break
     case "foldLight":
+      document.querySelector("label.foldLight").innerHTML=ev.target.value
       document.body.style.setProperty("--fold-color-light",ev.target.value)
       break
     case "diagram":
+      document.querySelector("label.diagram").innerHTML=ev.target.value
       document.body.style.setProperty("--diagram-color",ev.target.value)
       break
     case "diagramLight":
+      document.querySelector("label.diagramLight").innerHTML=ev.target.value
       document.body.style.setProperty("--diagram-color-light",ev.target.value)
       break
     default: break
